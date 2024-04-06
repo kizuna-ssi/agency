4c1277497d088ceee93b76dc1ca491c411b59727
kizuna-ssi <161543253+kizuna-ssi@users.noreply.github.com>
2024-04-06 15:12:31 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -120,3 +120,14 @@ $(function(){
         $('#navigation').load('navigation.html'); 
         });
 
+
+function calculate() {
+    var selectedOption = document.querySelector('input[name="course"]:checked').value;
+    if (selectedOption === "amount_fixed") {
+        document.querySelector(".open1").style.display = "block";
+        document.querySelector(".open2").style.display = "none";
+    } else if (selectedOption === "premium_fixed") {
+        document.querySelector(".open1").style.display = "none";
+        document.querySelector(".open2").style.display = "block";
+    }
+}
